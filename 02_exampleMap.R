3beb5d1cdf8cf4a6206dcf6eb775d2172ad466ba
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2019-03-20 22:45:43 -0500

    Update the script

--- 02_exampleMap.R
+++ 02_exampleMap.R
@@ -50,36 +50,7 @@ gg <- ggplot() +
   
 ggsave(plot = gg, filename = '../_maps/aptitudPromedioDpt.png', units = 'in', width = 9, height = 12, dpi = 300)
 
-
-# tdy <- broom::tidy(shp, region = 'MEAN_prc')
-# tdy <- tdy %>% mutate(group = as.numeric(group))
-# frt <- fortify(shp, 'MEAN_prc')
-# 
-# sft <- st_as_sf(shp)
-# sft <- sft %>% rename(Idoneidad_Porcentaje = MEAN_prc)
-# 
-# tm_shape(sft) +
-#   tm_polygons('Idoneidad_Porcentaje', palette = 'Greens')
-# tm_shape(mps) +
-#   tm_polygons('prc', palette = 'Greens')
-
-# ggplot(sft) +
-#   geom_sf(aes(fill = MEAN_prc)) +
-#   scale_fill_gradientn(low = '#red', high = 'green')
-# 
-# 
-# gg <- ggplot() +
-#   geom_polygon(data = shp, aes(long, lat, group = group, fill = MEAN_prc)) +
-#   xlab('Longitud') +
-#   ylab('Latitud') +
-#   scale_fill_continuous(low = 'red', high = 'green') +
-#   coord_equal()
-# 
-# 
-# ggsave(plot = gg, filename = 'suit_alto_prc.png', units = 'cm', height = 15, width = 9, dpi = 300)
-
-
-
+# End
 
 
 
